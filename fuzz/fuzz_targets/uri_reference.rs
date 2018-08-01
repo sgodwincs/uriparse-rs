15c972bcb2347a146626005c4df9cdcaad725c19
Scott Godwin <sgodwincs@gmail.com>
2018-08-01 00:40:40 -0700

    add fuzz

---
+++ fuzz/fuzz_targets/uri_reference.rs
@@ -0,0 +1,13 @@
+#![feature(try_from)]
+#![no_main]
+
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate uriparse;
+
+use std::convert::TryFrom;
+use uriparse::URIReference;
+
+fuzz_target!(|data: &[u8]| {
+    URIReference::try_from(data).ok();
+});
